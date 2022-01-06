70c1b3f1b5d65f83b9fd8ec859edf8e50aaa3334
yungyungGwon <gywn7724@gmail.com>
2022-01-06 18:22:44 +0900

    gitignore 적용안되는 문제 해결

--- src/init.js
+++
@@ -1,8 +0,0 @@
-import app from "./server.js";
-
-const PORT  = 4040;
-const handleListening = () => {
-    console.log(`Server Listen https:/localhost:${PORT}`);
-}
-
-app.listen(PORT, handleListening)
